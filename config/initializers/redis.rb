b549695aabecdcfed07e5e2e3bf962a04195cf97
andriy kravets <4kravets@gmail.com>
2019-03-31 14:57:14 +0200

    redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,12 +1,12 @@
-url = Rails.env.production? ? ENV["REDISCLOUD_URL"] : ENV["REDIS_URL"]
+# url = Rails.env.production? ? ENV["REDISCLOUD_URL"] : ENV["REDIS_URL"]
 
-if Rails.env.production?
-  Sidekiq.configure_server do |config|
-    config.redis = { url: url }
-  end
+# if Rails.env.production?
+#   Sidekiq.configure_server do |config|
+#     config.redis = { url: url }
+#   end
 
-  Sidekiq.configure_client do |config|
-    config.redis = { url: url }
-  end
-  $redis = Redis.new(:url => url)
-end
+#   Sidekiq.configure_client do |config|
+#     config.redis = { url: url }
+#   end
+#   $redis = Redis.new(:url => url)
+# end
