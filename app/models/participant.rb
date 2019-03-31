9c601a56fe70d4ffd8eb37281c7e3864bffe8790
andriy kravets <4kravets@gmail.com>
2019-03-31 15:18:06 +0200

    updating model

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,11 +1,11 @@
 class Participant < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :trip
-  after_create :send_invite_email
+  # after_create :send_invite_email
 
-  private
+  # private
 
-  def send_invite_email
-    UserMailer.invite(self).deliver_later
-  end
+  # def send_invite_email
+  #   UserMailer.invite(self).deliver_later
+  # end
 end
